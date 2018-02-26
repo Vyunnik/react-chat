65f6e042844fa278f88e39a503957158f5c7d569
Vadim <vadjuk1996@gmail.com>
2018-02-26 22:30:57 +0300

    Add components, routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import 'typeface-roboto';
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 );
 
 registerServiceWorker();
