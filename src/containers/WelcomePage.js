b35ab7c0bf45dc6b3b8450c6d04a4b4c7cfac8fb
Vyunnik <vadjuk1996@gmail.com>
2018-03-02 01:48:50 +0300

    Add Redux and devtools

---
+++ src/containers/WelcomePage.js
@@ -0,0 +1,20 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { signup, login } from '../actions';
+import WelcomePage from '../components/WelcomePage';
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    //the following lines are the same but without bindAction
+    //signup: (username, password) => dispatch(signup(username, password)),
+    //login: (username, password) => dispatch(login(username, password)),
+    signup, login,
+}, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(WelcomePage);
